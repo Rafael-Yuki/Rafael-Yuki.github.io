059251b831921d7f0b19f4dc3d38cc348bb9f8f6
Rafael-Yuki <rafael.tobaru@edu.unifil.br>
2023-09-21 12:37:11 -0300

    eslint

---
+++ eslint.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // As regras ESLint vão aqui
+    // ...
+    rules: {
+      // Exemplos de regras (você pode adicionar mais):
+      'no-console': 'error', // Impede o uso de console.log
+      'semi': ['error', 'always'], // Exige ponto e vírgula no final das linhas
+    },
+  };
+  
